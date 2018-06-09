d680d02e7419b32e27a07bafccf86a4e88183980
Bill Rich <bill.rich@gmail.com>
2018-06-08 23:58:54 -0700

    wip

--- vsphere/internal/helper/vappcontainer/vappcontainer_helper.go
+++ vsphere/internal/helper/vappcontainer/vappcontainer_helper.go
@@ -45,6 +45,19 @@ func FromID(client *govmomi.Client, id string) (*object.VirtualApp, error) {
 	return obj.(*object.VirtualApp), nil
 }
 
+// EntityFromKey locates a VAppEntityConfigInfo within a vApp container by the
+// string value of its key.
+func EntityFromKey(key string, c *mo.VirtualApp) *types.VAppEntityConfigInfo {
+	log.Printf("[DEBUG] Locating VApp entity with key %s", key)
+	for _, e := range c.VAppConfig.EntityConfig {
+		if e.Key.Value == key {
+			log.Printf("[DEBUG] vApp entity found: %s", key)
+			return &e
+		}
+	}
+	return nil
+}
+
 // Properties returns the VirtualApp managed object from its higher-level
 // object.
 func Properties(obj *object.VirtualApp) (*mo.VirtualApp, error) {
